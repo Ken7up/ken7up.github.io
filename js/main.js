abc600015de900097be0a66ef95e04127170a463
Jong Ken <kentrn6789@gmail.com>
2026-03-19 12:41:55 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
-import BootScene from './scenes/BootScene.js';
-import GameScene from './scenes/GameScene.js';
-
 const config = {
     type: Phaser.AUTO,
     scale: {
-        // Đã đổi sang ENVELOP để tràn viền
-        mode: Phaser.Scale.ENVELOP, 
+        mode: Phaser.Scale.FIT, // Tự động co giãn vừa với màn hình (rất quan trọng cho mobile)
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 720,
-        height: 1280
+        width: 800,
+        height: 300,
+        parent: 'game-container'
     },
-    parent: 'game-container',
-    backgroundColor: '#87CEEB',
-    scene: [BootScene, GameScene],
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 0 },
-            // Đổi false sang true để hiện khung va chạm
+            gravity: { y: 1200 }, // Trọng lực kéo khủng long xuống
             debug: false
         }
-    }
+    },
+    backgroundColor: '#f7f7f7',
+    scene: [BootScene, GameScene]
 };
 
 const game = new Phaser.Game(config);
