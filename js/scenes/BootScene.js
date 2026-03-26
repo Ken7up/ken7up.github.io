e4b9e9b627385f3c8de3b319bdde8d2f46a7b737
Jong Ken <kentrn6789@gmail.com>
2026-03-26 11:29:26 +0700

    Update BootScene.js

--- js/scenes/BootScene.js
+++ js/scenes/BootScene.js
@@ -15,6 +15,21 @@ class BootScene extends Phaser.Scene {
             frameWidth: 32, 
             frameHeight: 32
         });
+
+        // TẢI NỀN NÚI
+        this.load.image('mountain', 'assets/images/mountain.png');
+
+        // TẢI NỀN MÂY
+        this.load.image('cloud', 'assets/images/cloud.png');
+
+        // TẢI ÂM THANH NHẢY:
+        this.load.audio('sound_jump', 'assets/audio/jump.ogg');
+
+        // TẢI ÂM THANH TIẾP ĐẤT
+        this.load.audio('sound_jumpland', 'assets/audio/jumpland.wav');
+
+        // ÂM THANH LƯỚT
+        this.load.audio('sound_dash', 'assets/audio/dash.wav');
     }
 
     create() {
