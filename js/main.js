372bb0a3fcc6f3e2c466aea91acad98617c2ad1f
Jong Ken <kentrn6789@gmail.com>
2026-04-21 17:51:55 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ import GameScene from './scenes/game-scene.js';
 const config = {
     type: Phaser.AUTO,
     scale: {
-        // Đổi từ ENVELOP sang FIT để không bị cắt trên/dưới trên máy tính
+        // Sử dụng FIT để Phaser tự co giãn game vừa với màn hình nhưng KHÔNG làm méo tỷ lệ
         mode: Phaser.Scale.FIT, 
+        // Căn giữa game trên trình duyệt máy tính
         autoCenter: Phaser.Scale.CENTER_BOTH,
+        // Cố định kích thước thiết kế gốc
         width: 720,
         height: 1280
     },
